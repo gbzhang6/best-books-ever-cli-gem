a614aa2d8ef8439fc210df8318639df436799d2a
gbzhang6 <guibzhang@gmail.com>
2017-11-01 14:00:41 -0400

    created scraper method

---
+++ lib/best_books_ever/scraper.rb
@@ -0,0 +1,16 @@
+class BestBooks::Scraper
+
+  def get_page
+    Nokogiri::HTML(open('https://www.goodreads.com/list/show/1.Best_Books_Ever'))
+  end
+
+  def scrape_books_attributes
+    self.get_page.css("table.tableList.js-dataTooltip")
+  end
+
+  def make_booklist
+    scrape_books_attributes.each do |b|
+      BestBooks::Booklist.new_from_top_page(b)
+    end
+  end
+end
