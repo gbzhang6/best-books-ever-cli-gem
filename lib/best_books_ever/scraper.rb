5548ffa9dcbb4432a508c506a9dcb88b26ffb488
gbzhang6 <guibzhang@gmail.com>
2017-11-02 23:04:55 -0400

    created hash method and values

--- lib/best_books_ever/scraper.rb
+++ lib/best_books_ever/scraper.rb
@@ -4,12 +4,19 @@ class BestBooksEver::Scraper
     doc = Nokogiri::HTML(open("https://www.goodreads.com/list/show/1.Best_Books_Ever"))
     book_attributes = []
     doc.css("table.tableList tr").each do |book|
-    binding.pry
-    # create hash
-    # give hash key / values
-    # push hash into book attributes
+      binding.pry
+      # create hash
+      book_hash = Hash.new
+      # give hash key / values
+      book_hash[:name] = book.css("a.bookTitle span").text
+      book_hash[:author] = book.css("a.authorName span").text
+      book_hash[:url] = book.css("a.bookTitle").attr ("href").value
+      book_hash[:position] = ("td.number").text
+      # push hash into book attributes
+      book_attributes << book_hash
     end
     #return book attributes
+    book_attributes
   end
 
   def make_booklist
